8606d812d7ee41f435a55229b6f30445c9da4e1d
Cian <cnlynch20@gmail.com>
2021-07-23 13:39:46 +1000

    Forgot to add the multiple choice lol

--- src/test/java/com/example/restservice/ApplicationTests.java
+++ src/test/java/com/example/restservice/ApplicationTests.java
@@ -1,13 +1,32 @@
-package com.example.restservice;
+import static org.hamcrest.Matchers.containsString;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
+import com.example.restservice.MyName;
+import com.example.restservice.MyNameController;
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
-class ApplicationTests {
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.web.servlet.MockMvc;
+
+@WebMvcTest(MyNameController.class)
+public class ApplicationTests {
+
+	@Autowired
+	private MockMvc mockMvc;
+
+	@MockBean
+	private MyName service;
 
 	@Test
-	void contextLoads() {
+	public void greetingShouldReturnMessageFromService() throws Exception {
+		when(service.greet()).thenReturn("Hello, Mock");
+		this.mockMvc.perform(get("/greeting")).andDo(print()).andExpect(status().isOk())
+				.andExpect(content().string(containsString("Hey Cian!")));
 	}
-
 }
